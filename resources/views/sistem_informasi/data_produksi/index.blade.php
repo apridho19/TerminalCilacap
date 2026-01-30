57eb1e17c96d6787118cd411fa71854e86f529cc
apridho19 <apridhomoeljo@gmail.com>
2026-01-30 13:55:31 +0700

    sidebar udah bisa digeser

--- resources/views/sistem_informasi/data_produksi/index.blade.php
+++ resources/views/sistem_informasi/data_produksi/index.blade.php
@@ -944,16 +944,6 @@
     }
 </style>
 
-<!-- CORE PLUGINS-->
-<script src="./assets/vendors/jquery/dist/jquery.min.js" type="text/javascript"></script>
-<script src="./assets/vendors/popper.js/dist/umd/popper.min.js" type="text/javascript"></script>
-<script src="./assets/vendors/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="./assets/vendors/metisMenu/dist/metisMenu.min.js" type="text/javascript"></script>
-<script src="./assets/vendors/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-<!-- PAGE LEVEL PLUGINS-->
-<script src="./assets/vendors/DataTables/datatables.min.js" type="text/javascript"></script>
-<!-- CORE SCRIPTS-->
-<script src="assets/js/app.min.js" type="text/javascript"></script>
 <!-- PAGE LEVEL SCRIPTS-->
 <script type="text/javascript">
     $(function() {
